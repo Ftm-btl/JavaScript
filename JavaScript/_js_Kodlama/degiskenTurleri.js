3388d09ac6503c4777b16972de74df181bd67770
Ftm-btl <88613684+Ftm-btl@users.noreply.github.com>
2025-02-20 16:54:56 +0300

    Değişken Türleri

---
+++ JavaScript/_js_Kodlama/degiskenTurleri.js
@@ -0,0 +1,39 @@
+//Değişken Türleri
+
+var urunAdi = "Iphone 16"; //string
+var urunFiyat = "70000"; //string
+
+var urunFiyat1 = 70000; //number
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+console.log(typeof urunFiyat1);
+
+var sayi1 = '10';
+var sayi2 = '20';
+
+console.log(sayi1 + sayi2); //1020
+
+var ad = 'Fatma Betül';
+var soyad = 'Çiftci';
+
+console.log(ad + " " + soyad);
+
+ad="Nebahat";
+
+console.log(ad + " " + soyad); //Fatma Betül Çiftci 
+
+var sayi1 = 10;
+var sayi2 = 20;
+
+console.log(sayi1 + sayi2); //30
+
+var sinavNotu = 40;
+var basarilimi = (sinavNotu >= 50); //boolean => true/false
+
+console.log(basarilimi);
+
+var yas; 
+
+console.log(yas); //undefined
+console.log(typeof yas);
