e6f5d429995e3f1f7a38dfadce654f65aaf97a9f
Ftm-btl <88613684+Ftm-btl@users.noreply.github.com>
2025-02-20 16:36:28 +0300

    değişkenler

---
+++ JavaScript/_js_Kodlama/script.js
@@ -0,0 +1,33 @@
+//değişkenler
+
+var a=5500;
+var b=5000;
+var c=6000;
+var d=7000;
+var kdv=1.8;
+
+console.log(a * kdv);
+
+a=6000;
+
+console.log(a * kdv);
+console.log(b * kdv);
+console.log(c * kdv);
+console.log(d * kdv);
+
+//Değişken tanımlama kuralları
+//Türkçe karakter kullanılmaz
+var urunFiyat=6000;
+
+//Arada boşluk bırakılmaz
+//var urun Fiyat = 6000;
+ var urun_adi="Elma";
+
+//sayı ile başlayamaz
+//var 1urunFiyat=6000;
+var urunFiyat1=6000;
+var urunFiyat2=6000;
+
+//komut isimleri kullanılamaz
+//var var=6000;
+//var function=6000;
