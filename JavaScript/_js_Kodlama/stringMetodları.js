1d25772ad247308f33ff457e52786d4b432252cb
Ftm-btl <88613684+Ftm-btl@users.noreply.github.com>
2025-02-21 15:47:07 +0300

    String Metodları

---
+++ JavaScript/_js_Kodlama/stringMetodları.js
@@ -0,0 +1,28 @@
+// String Metodları
+
+var kursAdi = "Komple Uygulamalı Web Geliştirme Eğitimi";
+var sonuc;
+
+sonuc = kursAdi.toLowerCase(); // Küçük harfe çevirir
+sonuc = kursAdi.toUpperCase(); // Büyük harfe çevirir
+sonuc = kursAdi.length; // Karakter sayısını verir
+sonuc = kursAdi[0];
+sonuc = kursAdi.slice(0, 5); // 0. karakterden başla 5. karaktere kadar al
+sonuc = kursAdi.slice(10); // 10. karakterden başla sonuna kadar al
+sonuc = kursAdi.slice(-5); // Son 5 karakteri al
+sonuc = kursAdi.slice(-10,-5); // Son 10 karakterden başla son 5 karaktere kadar al
+
+sonuc = kursAdi.substring(0,6); // 0. karakterden başla 6. karaktere kadar al
+sonuc = kursAdi.substring(10); // 10. karakterden başla sonuna kadar al
+
+sonuc = kursAdi.replace("Eğitimi", "Kursu"); // Eğitimi kelimesini Kursu ile değiştir
+
+sonuc = kursAdi.trim(); // Başındaki ve sonundaki boşlukları siler
+sonuc = kursAdi.trimEnd(); // Sonundaki boşlukları siler
+sonuc = kursAdi.trimStart(); // Başındaki boşlukları siler  
+
+sonuc = kursAdi.indexOf("Web"); // Aranan kelimenin başlangıç indexini verir
+sonuc = kursAdi.split(" "); // Boşluk ile ayırarak diziye çevirir
+sonuc = kursAdi.split(" ")[1]; // Boşluk ile ayırarak diziye çevirir ve 1. elemanı alır
+
+console.log(sonuc);
