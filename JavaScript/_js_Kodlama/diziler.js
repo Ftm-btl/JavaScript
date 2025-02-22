f1523a98376ed123e151fd093f7c548932f68053
Ftm-btl <88613684+Ftm-btl@users.noreply.github.com>
2025-02-22 23:12:19 +0300

    diziler

---
+++ JavaScript/_js_Kodlama/diziler.js
@@ -0,0 +1,14 @@
+// Diziler
+
+let urun1="Iphone 15";
+let urun2="Iphone 16";
+let urun3="Iphone 17";
+
+let urunler = ["Iphone 15", "Iphone 16", "Iphone 17"];
+let fiyatlar = [ 50000, 60000, 70000];
+let renkler = ["gold", "silver", "black"];
+
+let sonuc = `${urunler[0]} - ${fiyatlar[0]}-${renkler[0]}`; // Iphone 15
+
+
+console.log(sonuc);
