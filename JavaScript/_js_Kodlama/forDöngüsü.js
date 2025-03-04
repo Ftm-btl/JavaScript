cd3169c24a42b14d7bd9815b0aded1fe0afdc223
Ftm-btl <88613684+Ftm-btl@users.noreply.github.com>
2025-03-04 23:59:12 +0300

    Update forDöngüsü.js

--- JavaScript/_js_Kodlama/forDöngüsü.js
+++ JavaScript/_js_Kodlama/forDöngüsü.js
@@ -14,8 +14,13 @@ for(let i = 0; i <= 100; i++)
 }
 
 let sayilar=[1,2,3,4,5,6,8,9,13];
+let toplam=0;
 
 for(let i = 0; i< sayilar.length;i++ )
 {
     console.log(sayilar[i]);
+
+    toplam += sayilar[i];
 }
+
+console.log(toplam);
