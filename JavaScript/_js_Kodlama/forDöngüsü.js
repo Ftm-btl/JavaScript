08f431036dd04b09a2d12854335190e641c2c0bf
Ftm-btl <88613684+Ftm-btl@users.noreply.github.com>
2025-03-04 23:57:33 +0300

    update forDöngüsü.js

--- JavaScript/_js_Kodlama/forDöngüsü.js
+++ JavaScript/_js_Kodlama/forDöngüsü.js
@@ -1,1 +1,21 @@
 //For Döngüsü
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// //...
+
+// console.log(100);
+
+for(let i = 0; i <= 100; i++)
+{
+    console.log(i);
+}
+
+let sayilar=[1,2,3,4,5,6,8,9,13];
+
+for(let i = 0; i< sayilar.length;i++ )
+{
+    console.log(sayilar[i]);
+}
